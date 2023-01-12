b5efa6171b3effc06e5e2f30582f92dcceea503d
Siamul <siamulkarim@gmail.com>
2023-01-12 01:35:37 -0500

    'First_commit'

---
+++ visualize_images.py
@@ -0,0 +1,30 @@
+from polar_normalization import PolarNormalization
+import argparse
+import torch
+import os
+from PIL import Image
+from tqdm import tqdm
+
+def visualize_images(args):
+    if not os.path.exists(args.vis_dir):
+        os.mkdir(args.vis_dir)
+    polar_normalizer = PolarNormalization(mask_net_path = args.mask_net_path, circle_net_path = args.circle_net_path, device = torch.device('cpu'))
+    for imagename in tqdm(os.listdir(args.image_dir)):
+        if imagename.endswith(('jpg', 'jpeg', 'bmp', 'png', 'gif')):
+            imagepath = os.path.join(args.image_dir, imagename)
+            image_pil = Image.open(imagepath).convert('L')
+            image_vis_pil = polar_normalizer.visualize_image(image_pil)
+            image_vis_pil.save(os.path.join(args.vis_dir, imagename))
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--cfg_path",
+                        type=str,
+                        default="cfg.yaml",
+                        help="path of the configuration file")
+    parser.add_argument('--mask_net_path', default='./nestedsharedatrousresunet-best.pth')
+    parser.add_argument('--circle_net_path', default='./nestedresunetparam-best.pth')
+    parser.add_argument('--image_dir', default='./test_gray_images')
+    parser.add_argument('--vis_dir', default='./vis_dir')
+    args = parser.parse_args()
+    visualize_images(args)
