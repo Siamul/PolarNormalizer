e987b49574b3c8937390656330f4647198299859
Siamul <siamulkarim@gmail.com>
2023-08-23 15:55:27 -0400

    Updated mask and circle parameter model

--- polar_normalization.py
+++ polar_normalization.py
@@ -9,118 +9,49 @@ from PIL import Image
 from skimage import img_as_bool
 from math import pi
 import math
+from torchvision import models
 
-class ResBlock(nn.Module):
-    def __init__(self, in_channels, middle_channels, out_channels):
+class fclayer(nn.Module):
+    def __init__(self, in_h = 8, in_w = 10, out_n = 6):
         super().__init__()
-        self.conv_res = nn.Conv2d(in_channels, out_channels, 1, stride =  1, bias = False)
-        
-        self.net = nn.Sequential(
-            nn.Conv2d(in_channels, middle_channels, 3, padding=1, bias=False),
-            nn.BatchNorm2d(middle_channels),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(middle_channels, out_channels, 3, padding=1, bias=False),
-            nn.BatchNorm2d(out_channels),
-            nn.ReLU(inplace=True)
-        )
-
+        self.in_h = in_h
+        self.in_w = in_w
+        self.out_n = out_n
+        self.fc_list = []
+        for i in range(out_n):
+            self.fc_list.append(nn.Linear(in_h*in_w, 1))
+        self.fc_list = nn.ModuleList(self.fc_list)
     def forward(self, x):
-        res = self.conv_res(x)
-        x = self.net(x)
-        x = (x + res) * (1 / math.sqrt(2))
-        return x
+        x = x.reshape(-1, 6, self.in_h, self.in_w)
+        outs = []
+        for i in range(self.out_n):
+            outs.append(self.fc_list[i](x[:, i, :, :].reshape(-1, self.in_h*self.in_w)))
+        out = torch.cat(outs, 1)
+        return out
 
-class NestedResUNetParam(nn.Module):
-    def __init__(self, num_channels, num_params=6, width=32, resolution=(240, 320)):
+class conv(nn.Module):
+    def __init__(self, in_channels=512, out_n = 6):
         super().__init__()
-
-        self.resolution = resolution
-        nb_filter = [width, width*2, width*4, width*8, width*16]
-
-        self.pool = nn.MaxPool2d(2, stride=2)
-        self.up = nn.Upsample(scale_factor=2, mode='nearest')
-
-        self.conv0_0 = ResBlock(num_channels, nb_filter[0], nb_filter[0])
-        self.conv1_0 = ResBlock(nb_filter[0], nb_filter[1], nb_filter[1])
-        self.conv2_0 = ResBlock(nb_filter[1], nb_filter[2], nb_filter[2])
-        self.conv3_0 = ResBlock(nb_filter[2], nb_filter[3], nb_filter[3])
-        self.conv4_0 = ResBlock(nb_filter[3], nb_filter[4], nb_filter[4])
-
-        self.conv0_1 = ResBlock(nb_filter[0]+nb_filter[1], nb_filter[0], nb_filter[0])
-        self.conv1_1 = ResBlock(nb_filter[1]+nb_filter[2], nb_filter[1], nb_filter[1])
-        self.conv2_1 = ResBlock(nb_filter[2]+nb_filter[3], nb_filter[2], nb_filter[2])
-        self.conv3_1 = ResBlock(nb_filter[3]+nb_filter[4], nb_filter[3], nb_filter[3])
-
-        self.conv0_2 = ResBlock(nb_filter[0]*2+nb_filter[1], nb_filter[0], nb_filter[0])
-        self.conv1_2 = ResBlock(nb_filter[1]*2+nb_filter[2], nb_filter[1], nb_filter[1])
-        self.conv2_2 = ResBlock(nb_filter[2]*2+nb_filter[3], nb_filter[2], nb_filter[2])
-
-        self.conv0_3 = ResBlock(nb_filter[0]*3+nb_filter[1], nb_filter[0], nb_filter[0])
-        self.conv1_3 = ResBlock(nb_filter[1]*3+nb_filter[2], nb_filter[1], nb_filter[1])
-
-        self.conv0_4 = ResBlock(nb_filter[0]*4+nb_filter[1], nb_filter[0], nb_filter[0])
-        
-        self.xyr_input = ResBlock(num_channels, nb_filter[0], nb_filter[0])      
-        self.xyr_conv1 = ResBlock(nb_filter[0]*5, nb_filter[0]*4, nb_filter[0]*4)
-        self.xyr_conv2 = ResBlock(nb_filter[0]*4, nb_filter[0]*3, nb_filter[0]*3)
-        self.xyr_conv3 = ResBlock(nb_filter[0]*3, nb_filter[0]*2, nb_filter[0]*2)
-        self.xyr_conv4 = ResBlock(nb_filter[0]*2, nb_filter[0], nb_filter[0])
-        self.xyr_linear = nn.Sequential(
-                              nn.Flatten(),
-                              nn.Linear(nb_filter[0]*int(resolution[0]/16)*int(resolution[1]/16), int(resolution[0]/16)*int(resolution[1]/16)),
-                              nn.ReLU(),
-                              nn.Linear(int(resolution[0]/16)*int(resolution[1]/16), num_params)
-                          )
-
-
-    def forward(self, input):
-        x0_0 = self.conv0_0(input)
-        x1_0 = self.conv1_0(self.pool(x0_0))
-        x0_1 = self.conv0_1(torch.cat([x0_0, self.up(x1_0)], 1))
-
-        x2_0 = self.conv2_0(self.pool(x1_0))
-        x1_1 = self.conv1_1(torch.cat([x1_0, self.up(x2_0)], 1))
-        x0_2 = self.conv0_2(torch.cat([x0_0, x0_1, self.up(x1_1)], 1))
-
-        x3_0 = self.conv3_0(self.pool(x2_0))
-        x2_1 = self.conv2_1(torch.cat([x2_0, self.up(x3_0)], 1))
-        x1_2 = self.conv1_2(torch.cat([x1_0, x1_1, self.up(x2_1)], 1))
-        x0_3 = self.conv0_3(torch.cat([x0_0, x0_1, x0_2, self.up(x1_2)], 1))
-
-        x4_0 = self.conv4_0(self.pool(x3_0))
-        x3_1 = self.conv3_1(torch.cat([x3_0, self.up(x4_0)], 1))
-        x2_2 = self.conv2_2(torch.cat([x2_0, x2_1, self.up(x3_1)], 1))
-        x1_3 = self.conv1_3(torch.cat([x1_0, x1_1, x1_2, self.up(x2_2)], 1))
-        x0_4 = self.conv0_4(torch.cat([x0_0, x0_1, x0_2, x0_3, self.up(x1_3)], 1))
-        
-        xyr0 = self.xyr_input(input)
-        xyr1 = self.xyr_conv1(torch.cat([xyr0, x0_1, x0_2, x0_3, x0_4], 1)) #240x320
-        xyr2 = self.xyr_conv2(self.pool(xyr1)) #120x160
-        xyr3 = self.xyr_conv3(self.pool(xyr2)) #60x80
-        xyr4 = self.xyr_conv4(self.pool(xyr3)) #30x40
-        xyr5 = self.xyr_linear(self.pool(xyr4)) #15x20
-          
-        return xyr5
+        self.conv = nn.Conv2d(in_channels, out_n, kernel_size=1, stride=1, padding='same')
+    def forward(self, x):
+        x = self.conv(x)
+        return x
 
 class SharedAtrousConv2d(nn.Module):
     def __init__(self, in_channels, out_channels, bias=True):
         super().__init__()
-        self.weights = nn.Parameter(torch.rand(out_channels, in_channels, 3, 3))
-        nn.init.kaiming_normal_(self.weights, mode='fan_out', nonlinearity='relu')
+        self.weights = nn.Parameter(torch.rand(int(out_channels/2), in_channels, 3, 3))
+        nn.init.kaiming_uniform_(self.weights, mode='fan_out', nonlinearity='relu')
         if bias:
-            self.bias1 = nn.Parameter(torch.rand(out_channels))
-            nn.init.zeros_(self.bias1)
-            self.bias2 = nn.Parameter(torch.rand(out_channels))
-            nn.init.zeros_(self.bias2)
+            self.bias1 = nn.Parameter(torch.zeros(int(out_channels/2)))
+            self.bias2 = nn.Parameter(torch.zeros(int(out_channels/2)))
         else:
             self.bias1 = None
             self.bias2 = None
-        self.join_net = nn.Conv2d(out_channels * 2, out_channels, kernel_size=1, stride=1, padding='same', bias=bias)
-        nn.init.kaiming_normal_(self.join_net.weight, mode='fan_in', nonlinearity='relu')
     def forward(self, x):
         x1 = nn.functional.conv2d(x, self.weights, stride=1, padding='same', bias=self.bias1)
         x2 = nn.functional.conv2d(x, self.weights, stride=1, padding='same', dilation=2, bias=self.bias2)
-        x3 = self.join_net(torch.cat([x1, x2], 1))
+        x3 = torch.cat([x1, x2], 1)
         return x3
 
 class SharedAtrousResBlock(nn.Module):
@@ -190,20 +121,20 @@ class NestedSharedAtrousResUNet(nn.Module):
 
     def forward(self, input):
         
-        x0_0 = self.conv0_0(input)
-        x1_0 = self.conv1_0(self.pool(x0_0))
+        x0_0 = self.conv0_0(input) #320x240
+        x1_0 = self.conv1_0(self.pool(x0_0)) #160x120
         x0_1 = self.conv0_1(torch.cat([x0_0, self.up(x1_0)], 1))
 
-        x2_0 = self.conv2_0(self.pool(x1_0))
+        x2_0 = self.conv2_0(self.pool(x1_0)) #80x60
         x1_1 = self.conv1_1(torch.cat([x1_0, self.up(x2_0)], 1))
         x0_2 = self.conv0_2(torch.cat([x0_0, x0_1, self.up(x1_1)], 1))
 
-        x3_0 = self.conv3_0(self.pool(x2_0))
+        x3_0 = self.conv3_0(self.pool(x2_0)) #40x30
         x2_1 = self.conv2_1(torch.cat([x2_0, self.up(x3_0)], 1))
         x1_2 = self.conv1_2(torch.cat([x1_0, x1_1, self.up(x2_1)], 1))
         x0_3 = self.conv0_3(torch.cat([x0_0, x0_1, x0_2, self.up(x1_2)], 1))
 
-        x4_0 = self.conv4_0(self.pool(x3_0))
+        x4_0 = self.conv4_0(self.pool(x3_0)) #20x15
         x3_1 = self.conv3_1(torch.cat([x3_0, self.up(x4_0)], 1))
         x2_2 = self.conv2_2(torch.cat([x2_0, x2_1, self.up(x3_1)], 1))
         x1_3 = self.conv1_3(torch.cat([x1_0, x1_1, x1_2, self.up(x2_2)], 1))
@@ -214,14 +145,16 @@ class NestedSharedAtrousResUNet(nn.Module):
         return output
 
 class PolarNormalization(object):
-    def __init__(self, polar_height = 64, polar_width = 512, mask_net_path = './nestedsharedatrousresunet-best.pth', circle_net_path = './nestedresunetparam-best.pth', device=torch.device('cuda')):
+    def __init__(self, polar_height = 64, polar_width = 512, mask_net_path = './nestedsharedatrousresunet-217-0.027828-maskIoU-0.938739.pth', circle_net_path = './resnet34-1583-0.045002-maskIoU-0.93717.pth', device=torch.device('cuda')):
         self.polar_height = polar_height
         self.polar_width = polar_width
         self.circle_net_path = circle_net_path
         self.mask_net_path = mask_net_path
         self.device = device
         self.NET_INPUT_SIZE = (320,240)
-        self.circle_model = NestedResUNetParam(1, 6)
+        self.circle_model = models.resnet34()
+        self.circle_model.avgpool = conv(in_channels=512, out_n=6)
+        self.circle_model.fc = fclayer(out_n=6)
         try:
             self.circle_model.load_state_dict(torch.load(self.circle_net_path, map_location=self.device))
         except AssertionError:
@@ -230,7 +163,7 @@ class PolarNormalization(object):
                     map_location = lambda storage, loc: storage))
         self.circle_model = self.circle_model.to(self.device)
         self.circle_model.eval()
-        self.mask_model = NestedSharedAtrousResUNet(1, 1)
+        self.mask_model = NestedSharedAtrousResUNet(1, 1, width=64, resolution=(self.NET_INPUT_SIZE[1], self.NET_INPUT_SIZE[0]))
         try:
             self.mask_model.load_state_dict(torch.load(self.mask_net_path, map_location=self.device))
         except AssertionError:
@@ -241,12 +174,12 @@ class PolarNormalization(object):
         self.mask_model.eval()
         self.input_transform = Compose([
             ToTensor(),
-            Normalize(mean=(0.5791223733793273,), std=(0.21176097694558188,))
+            Normalize(mean=(0.5,), std=(0.5,))
         ])
     
     def getMask(self, image):
         w,h = image.size
-        image = cv2.resize(np.array(image), self.NET_INPUT_SIZE, cv2.INTER_LINEAR_EXACT)
+        image = cv2.resize(np.array(image), self.NET_INPUT_SIZE, cv2.INTER_CUBIC)
 
         mask_logit_t = self.mask_model(Variable(self.input_transform(image).unsqueeze(0).to(self.device)))[0]
         mask_t = torch.where(torch.sigmoid(mask_logit_t) > 0.5, 255, 0)
@@ -258,20 +191,20 @@ class PolarNormalization(object):
 
     def circApprox(self, image):
         w,h = image.size
-        image = cv2.resize(np.array(image), self.NET_INPUT_SIZE, cv2.INTER_LINEAR_EXACT)
-        w_mult = w/self.NET_INPUT_SIZE[0]
-        h_mult = h/self.NET_INPUT_SIZE[1]
 
-        inp_xyr_t = self.circle_model(Variable(self.input_transform(image).unsqueeze(0).to(self.device)))
+        image = cv2.resize(np.array(image), self.NET_INPUT_SIZE, cv2.INTER_CUBIC)
+        with torch.no_grad():
+            inp_xyr_t = self.circle_model(Variable(self.input_transform(image).unsqueeze(0).repeat(1,3,1,1).to(self.device)))
 
         #Circle params
+        diag = math.sqrt(w**2 + h**2)
         inp_xyr = inp_xyr_t.tolist()[0]
-        pupil_x = int(inp_xyr[0] * w_mult)
-        pupil_y = int(inp_xyr[1] * h_mult)
-        pupil_r = int(inp_xyr[2] * max(w_mult, h_mult))
-        iris_x = int(inp_xyr[3] * w_mult)
-        iris_y = int(inp_xyr[4] * h_mult)
-        iris_r = int(inp_xyr[5] * max(w_mult, h_mult))
+        pupil_x = int(inp_xyr[0] * w)
+        pupil_y = int(inp_xyr[1] * h)
+        pupil_r = int(inp_xyr[2] * 0.5 * 0.8 * diag)
+        iris_x = int(inp_xyr[3] * w)
+        iris_y = int(inp_xyr[4] * h)
+        iris_r = int(inp_xyr[5] * 0.5 * diag)
 
         return np.array([pupil_x,pupil_y,pupil_r]), np.array([iris_x,iris_y,iris_r])
 
@@ -284,18 +217,30 @@ class PolarNormalization(object):
         gridx = ((gridx + 1) / 2 * W - 0.5) / (W - 1) * 2 - 1
         gridy = ((gridy + 1) / 2 * H - 0.5) / (H - 1) * 2 - 1
         newgrid = torch.stack([gridx, gridy], dim=-1)
-        return torch.nn.functional.grid_sample(input, newgrid, mode=interp_mode, align_corners=True)
+        return torch.nn.functional.grid_sample(input, newgrid, mode=interp_mode, align_corners=False)
 
     def visualize_image(self, image):
         mask = self.getMask(image)
         pupil_xyr, iris_xyr = self.circApprox(image)
         imVis = np.stack((np.array(image),)*3, axis=-1)
         #print(imVis.shape)
-        imVis[:,:,1] = np.clip(imVis[:,:,1] + 96*mask,0,255)
-        imVis = cv2.circle(imVis, (pupil_xyr[0],pupil_xyr[1]), pupil_xyr[2], (0, 0, 255), 2)
-        imVis = cv2.circle(imVis, (iris_xyr[0],iris_xyr[1]), iris_xyr[2], (255, 0, 0), 2)
+        try:
+            imVis[:,:,1] = np.clip(imVis[:,:,1] + 0.25*mask,0,255)
+        except:
+            print("Mask could not be visualized.")
+            pass
+        try:
+            imVis = cv2.circle(imVis, (pupil_xyr[0],pupil_xyr[1]), pupil_xyr[2], (0, 0, 255), 2)
+        except:
+            print("Pupil circle could not be visualized, values are: ", pupil_xyr)
+            pass
+        try:
+            imVis = cv2.circle(imVis, (iris_xyr[0],iris_xyr[1]), iris_xyr[2], (255, 0, 0), 2)
+        except:
+            print("Iris circle could not be visualized, values are: ", iris_xyr)
+            pass
         imVis_pil = Image.fromarray(imVis)
-
+        
         return imVis_pil
 
     # Rubbersheet model-based Cartesian-to-polar transformation using bilinear interpolation from torch grid sample
--- visualize_images.py
+++ visualize_images.py
@@ -10,11 +10,11 @@ def visualize_images(args):
         os.mkdir(args.vis_dir)
     polar_normalizer = PolarNormalization(mask_net_path = args.mask_net_path, circle_net_path = args.circle_net_path, device = torch.device('cpu'))
     for imagename in tqdm(os.listdir(args.image_dir)):
-        if imagename.endswith(('jpg', 'jpeg', 'bmp', 'png', 'gif')):
+        if imagename.endswith(('jpg', 'jpeg', 'bmp', 'png', 'gif', 'pgm')):
             imagepath = os.path.join(args.image_dir, imagename)
             image_pil = Image.open(imagepath).convert('L')
             image_vis_pil = polar_normalizer.visualize_image(image_pil)
-            image_vis_pil.save(os.path.join(args.vis_dir, imagename))
+            image_vis_pil.save(os.path.join(args.vis_dir, imagename.split('.')[0] + '.png'))
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -22,8 +22,8 @@ if __name__ == '__main__':
                         type=str,
                         default="cfg.yaml",
                         help="path of the configuration file")
-    parser.add_argument('--mask_net_path', default='./nestedsharedatrousresunet-best.pth')
-    parser.add_argument('--circle_net_path', default='./nestedresunetparam-best.pth')
+    parser.add_argument('--mask_net_path', default='./nestedsharedatrousresunet-217-0.027828-maskIoU-0.938739.pth')
+    parser.add_argument('--circle_net_path', default='./resnet34-1583-0.045002-maskIoU-0.93717.pth')
     parser.add_argument('--image_dir', default='./test_gray_images')
     parser.add_argument('--vis_dir', default='./vis_dir')
     args = parser.parse_args()
